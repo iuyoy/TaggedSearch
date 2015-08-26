e700aaa02587d41d3e1f6dca2e92a7b16658ce43
iuyyoy <iuyyoy@163.com>
2015-08-26 17:45:35 +0800

    修改小问题

--- Generate_Tags/get_data.py
+++ Generate_Tags/get_data.py
@@ -28,7 +28,7 @@ class Get_word(object):
         return False
     #根据id得到word_name
     def get_word_name_by_id(self,word_id):
-        sql = "SELECT word_name FROM `%s` AS w,`%s` AS p  WHERE w.property = p.part_of_speech AND p.is_need = 1 AND w.id >= %d\
+        sql = "SELECT word_name FROM `%s` AS w,`%s` AS p  WHERE w.property = p.part_of_speech AND p.is_need = 1 AND w.word_id >= %d\
         LIMIT %d" %(words_table,word_properties_table,word_id,1)
         result = self.db.select(sql)
         if(result):
